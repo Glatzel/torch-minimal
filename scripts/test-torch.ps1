7d87795f89e4e39454b46ec5bfd53481d50be22c
Glatzel <893016099@qq.com>
2024-11-22 17:01:22 +0800

    ffix

--- scripts/test-torch.ps1
+++ scripts/test-torch.ps1
@@ -2,4 +2,4 @@ param( $pixi_python_env = "py311" )
 Set-Location $PSScriptRoot
 Set-Location ..
 
-pixi run -e "$pixi_python_env" python -c "import torch; print(torch.__version__); print(torch.__config__.show());"
+pixi run -e "$pixi_python_env" python -c "import torch; print(torch.__version__); print(torch.__config__.show());print(torch.__file__)"
