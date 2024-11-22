e7ff3aee926a0fe62d3a023939b1f3ba54dbbfe1
Glatzel <893016099@qq.com>
2024-11-22 15:45:17 +0800

    fix

--- scripts/build-torch.ps1
+++ scripts/build-torch.ps1
@@ -1,25 +1,26 @@
-param( $pixi_python_env = "py311",$version = "" )
-if($version -eq '')
-{
-    $version=gh release view -R pytorch/pytorch --json tagName -q .tagName
+param( $pixi_python_env = "py311", $version = "" )
+if ($version -eq '') {
+    $version = gh release view -R pytorch/pytorch --json tagName -q .tagName
 }
 Write-Host $version
 Set-Location $PSScriptRoot
 Set-Location ..
 
-$env:BUILD_TEST=0
-$env:USE_CUDA=0
-$env:USE_CUDNN=0
-$env:USE_DISTRIBUTED=0
-$env:USE_FBGEMM=1
-$env:USE_MKLDNN=0
-$env:USE_NCCL=0
-$env:USE_NINJA=1
-$env:USE_QNNPACK=0
-$env:USE_TENSORPIPE=0
-$env:USE_OPENMP=1
-
+$env:BUILD_TEST = "OFF"
+$env:USE_CUDA = "OFF"
+$env:USE_CUDNN = "OFF"
+$env:USE_DISTRIBUTED = "OFF"
+$env:USE_FBGEMM = "OFF"
+$env:USE_GLOO = "OFF"
+$env:USE_KINETO="OFF
+$env:USE_MKLDNN = "OFF"
+$env:USE_NCCL = "OFF"
+$env:USE_OPENMP = "ON"
+$env:USE_TENSORPIPE="OFF"
+$env:USE_XNNPACK = "OFF"
+"
 Set-Location pytorch
-$version > version.txt
+"$version" -replace "v","" > version.txt
+pixi run -e "$pixi_python_env" python setup.py clean
 pixi run -e "$pixi_python_env" python setup.py install
 pixi run -e "$pixi_python_env" python setup.py bdist_wheel
--- scripts/test-torch.ps1
+++ scripts/test-torch.ps1
@@ -2,4 +2,4 @@ param( $pixi_python_env = "py311" )
 Set-Location $PSScriptRoot
 Set-Location ..
 
-pixi run -e "$pixi_python_env" python -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
+pixi run -e "$pixi_python_env" python -c "import torch; print(torch.__version__); print(torch.__config__.show());"
